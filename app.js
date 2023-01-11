8316a4add1a74dda18c95074ef8593d9dae9b91c
Oluwaseun <oluwaseunadesina8@gmail.com>
2023-01-11 17:45:07 +0100

    Main functions done

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 6000;
+const path = require('path')
 
 const app = express()
 
 // enable body parser
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// set static folder    
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
